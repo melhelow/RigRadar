2a2402dc2aa0a84b5010370abaf454bd6bb4339e
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-08-31 02:37:16 +0000

    created a devise in seperate folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Permit extra Devise params (e.g., :name on sign up & account update)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,        keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
+# config/routes.rb
 Rails.application.routes.draw do
+  # Devise routes (using default controllers & app/views/devise/*)
   devise_for :drivers
 
-  # TEMP while Loads controller doesn't exist yet
+  # Auth-aware roots (for now both go to landing page)
   authenticated :driver do
     root "pages#home", as: :authenticated_root
   end
+
   unauthenticated do
     root "pages#home", as: :unauthenticated_root
   end
+
+  # Fallback so `root_path` always exists in views/layouts
+  root "pages#home"
+
 end
