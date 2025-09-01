dc78f657d7dd4214eb71c0c6ee74014e49dd9af9
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-09-01 00:59:02 +0000

    weight stations controller

--- app/controllers/weigh_stations_controller.rb
+++ app/controllers/weigh_stations_controller.rb
@@ -1,7 +1,16 @@
 class WeighStationsController < ApplicationController
+  before_action :authenticate_driver!
+
   def index
+    @q = params[:q].to_s.strip
+    scope = WeighStation.order(state: :asc, name: :asc)
+    if @q.present?
+      scope = scope.where("state ILIKE ? OR name ILIKE ?", "%#{@q}%", "%#{@q}%")
+    end
+    @weigh_stations = scope.limit(200)
   end
 
   def show
+    @weigh_station = WeighStation.find(params[:id])
   end
 end
