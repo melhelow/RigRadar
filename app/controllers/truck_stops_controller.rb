d12dc6a6dd4a8ba1a53b6f9b8196754aede692cd
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-09-02 05:07:40 +0000

    truck stop index controller

--- app/controllers/truck_stops_controller.rb
+++ app/controllers/truck_stops_controller.rb
@@ -1,5 +1,27 @@
 class TruckStopsController < ApplicationController
   def index
+    @providers = [
+      "Love's", "Pilot", "Flying J", "TA", "Petro", "AMBEST", "Pilot Flying J"
+    ]
+
+    scope = TruckStop.all
+
+    if params[:q].present?
+      q = "%#{params[:q].strip}%"
+      scope = scope.where(
+        "name ILIKE :q OR city ILIKE :q OR state ILIKE :q",
+        q: q
+      )
+    end
+
+    if params[:providers].present?
+      scope = scope.where(provider: params[:providers])
+    end
+
+    min_parking = params[:min_parking].to_i
+    scope = scope.where("parking_truck >= ?", min_parking) if min_parking > 0
+
+    @truck_stops = scope.order(:state, :city, :name).limit(200)
   end
 
   def show
