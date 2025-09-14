eb25a35c4d2863f9f7e82beafcdfe9cfd873f5fa
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-09-14 07:24:44 +0000

    valiation weigh_station

--- app/models/weigh_station.rb
+++ app/models/weigh_station.rb
@@ -27,5 +27,6 @@
 #
 class WeighStation < ApplicationRecord
 has_many :load_stops, as: :stoppable, dependent: :destroy, inverse_of: :stoppable
-  has_many :loads, through: :load_stops
+has_many :loads, through: :load_stops
+validates :lat, :lon, numericality: true, allow_nil: true
 end
