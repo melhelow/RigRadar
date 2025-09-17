e6a5df1e5fc2d91e9fa67be14156eaa0b82bb3a5
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2025-09-17 07:17:45 +0000

    rubocop -A: autocorrect offenses

--- db/migrate/20250917053055_cleanup_rest_areas.rb
+++ db/migrate/20250917053055_cleanup_rest_areas.rb
@@ -5,11 +5,11 @@ class CleanupRestAreas < ActiveRecord::Migration[7.1]
       municipality
       nhs_rest_stop
       state_number
-      objectid   # <-- correct name (NOT object_uid)
+      objectid # <-- correct name (NOT object_uid)
       x
       y
-      latitude   # safe even if it doesn't exist
-      longitude  # safe even if it doesn't exist
+      latitude # safe even if it doesn't exist
+      longitude # safe even if it doesn't exist
     ].each do |col|
       remove_column :rest_areas, col, if_exists: true
     end
