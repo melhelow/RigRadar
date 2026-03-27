c3e99cabf771abcb53543b93de2bc1ece7b149e2
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2026-03-27 05:26:19 +0000

    Add migrations and CSV imports to build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rake import:truckstops_verified[lib/csvs/truck_stops_verified.csv]
+FILE=lib/csvs/NTAD_Truck_Stop_Parking.csv bundle exec rake import:rest_areas
+FILE=lib/csvs/NTAD_Weigh_in_Motion_Stations.csv bundle exec rake import:weigh_stations
